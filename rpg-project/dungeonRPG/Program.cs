e735b1c6bd9c8e12de678f4ee97faa97d2c50c30
m00fi <gimmimc@gmail.com>
2026-03-25 14:24:59 +0100

    stage 2 - full implementation

--- rpg-project/dungeonRPG/Program.cs
+++ rpg-project/dungeonRPG/Program.cs
@@ -1,11 +1,12 @@
 ﻿using dungeonRPG;
 using dungeonRPG.UI;
 
+
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 var dungeonMenu = new DungeonSelectionMenu();
 var strategy = dungeonMenu.Display();
-
 var game = new Game(strategy);
+
 game.DisplayMenu();
 game.Run();
