894367eed1340bdb056c20a72bf2932fbc4cd36e
Michal Chrostowski <01199286@pw.edu.pl>
2026-04-07 16:43:45 +0200

    refactor: begin migrating hard-coded inputs

---
+++ rpg-project/dungeonRPG/Input/GameAction.cs
@@ -0,0 +1,16 @@
+namespace dungeonRPG.Input;
+
+public enum GameAction
+{
+    MoveUp,
+    MoveDown,
+    MoveLeft,
+    MoveRight,
+    Interact,
+    DropItem,
+    ToggleInventory,
+    UnequipAll,
+    SelectNext,
+    SelectPrev,
+    ExitGame
+}
