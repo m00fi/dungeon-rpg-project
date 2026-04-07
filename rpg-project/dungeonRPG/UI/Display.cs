2c9552fcaac6a196f15ba7440938c0f05b637b81
Michal Chrostowski <01199286@pw.edu.pl>
2026-04-07 16:47:02 +0200

    refactor: cleanup Display class

--- rpg-project/dungeonRPG/UI/Display.cs
+++ rpg-project/dungeonRPG/UI/Display.cs
@@ -7,72 +7,40 @@ namespace dungeonRPG.UI;
 public class Display
 {
     private int ScreenWidth => 82;
-    private int ScreenHeight => 0;
-    
+    private int SidePanelWidth => 60;
+
     public void Render(Room room, Player player, string? message, List<string> instructions)
     {
         Console.SetCursorPosition(0, 0);
 
+        RenderMapAndSidePanel(room, player);
+        RenderGroundItems(room, player);
+        RenderMessageBar(message);
+        RenderInstructions(instructions);
+        
+        for (int i = 0; i < 3; i++) Console.WriteLine(new string(' ', ScreenWidth));
+    }
+
+    // Main display methods
+
+    private void RenderMapAndSidePanel(Room room, Player player)
+    {
         List<(string Text, bool Highlight)> sidePanel = GenerateSidePanel(room, player);
 
         int mapDisplayHeight = Room.Height + 2; 
         int mapDisplayWidth = Room.Width + 2;
-
         int totalRows = Math.Max(mapDisplayHeight, sidePanel.Count);
 
         for (int y = 0; y < totalRows; y++)
         {
-            if (y == 0 || y == mapDisplayHeight - 1)
-            {
-                Console.Write("+" + new string('-', Room.Width) + "+");
-            }
-            else if (y > 0 && y < mapDisplayHeight - 1)
-            {
-                int mapY = y - 1;
-
-                Console.Write("|");
-
-                for (int x = 0; x < Room.Width; x++)
-                {
-                    if(x == player.X && mapY == player.Y)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.Write(player.Symbol);
-                        Console.ResetColor();
-                    }
-                    else
-                    {
-                        Console.Write(room.GetCell(x, mapY).GetSymbol());
-                    }
-                }
-
-                Console.Write("|");
-            }
-            else
-            {
-                Console.Write(new string(' ', mapDisplayWidth));
-            }
-
-            if (y < sidePanel.Count)
-            {
-                var line = sidePanel[y];
-                
-                if (line.Highlight)
-                {
-                    Console.BackgroundColor = ConsoleColor.White;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                }
-
-                Console.Write(line.Text.PadRight(40)); 
-                Console.ResetColor();
-            }
-            else
-            {
-                Console.Write(new string(' ', 40));
-            }
+            RenderMapRow(y, mapDisplayHeight, mapDisplayWidth, room, player);
+            RenderSidePanelRow(y, sidePanel);
             Console.WriteLine();
         }
+    }
 
+    private void RenderGroundItems(Room room, Player player)
+    {
         Cell currentCell = room.GetCell(player.X, player.Y);
         var itemDescriptions = currentCell.GetItemDescriptions();
         
@@ -85,134 +53,196 @@ public class Display
 
         if (itemDescriptions.Count == 0)
         {
-            bool isSelected = !player.IsInventoryActive;
-            if (isSelected)
-            {
-                Console.BackgroundColor = ConsoleColor.White;
-                Console.ForegroundColor = ConsoleColor.Black;
-                
-            }
-            string prefix = isSelected ? "> " : "  ";
-            string textToPrint = $"{prefix}~empty ";
-            Console.Write(textToPrint);
-            
-            Console.ResetColor();
-            
-            int padding = ScreenWidth - " ~empty".Length;
-            if (padding > 0) Console.WriteLine(new string(' ', padding));
-            else Console.WriteLine();
-            
-            for (int i = 1; i < maxVisibleGround; i++) 
-            {
-                Console.WriteLine(new string(' ', ScreenWidth));
-            }
-            Console.WriteLine(new string(' ', ScreenWidth));
+            RenderEmptyItemList(player.IsInventoryActive, maxVisibleGround);
         }
         else
         {
-            int selectedIdx = player.SelectedItemIndex;
-            
-            int startIdx = Math.Max(0, selectedIdx - (maxVisibleGround / 2));
-            if (startIdx + maxVisibleGround > itemDescriptions.Count)
-            {
-                startIdx = Math.Max(0, itemDescriptions.Count - maxVisibleGround);
-            }
-
-            for (int i = 0; i < maxVisibleGround; i++)
-            {
-                int itemIdx = startIdx + i;
-                if (itemIdx < itemDescriptions.Count)
-                {
-                    bool isSelected = !player.IsInventoryActive && (itemIdx == selectedIdx);
-                    
-                    if (isSelected)
-                    {
-                        Console.BackgroundColor = ConsoleColor.White;
-                        Console.ForegroundColor = ConsoleColor.Black;
-                    }
-
-                    string prefix = isSelected ? "> " : "  ";
-                    string textToPrint = $"{prefix}{itemDescriptions[itemIdx]} ";
-                    
-                    Console.Write(textToPrint);
-                    Console.ResetColor();
-
-                    int padding = 80 - textToPrint.Length;
-                    if (padding > 0) Console.WriteLine(new string(' ', padding));
-                    else Console.WriteLine();
-                }
-                else
-                {
-                    Console.WriteLine(new string(' ', ScreenWidth)); 
-                }
-            }
-
-            if (itemDescriptions.Count > maxVisibleGround)
-            {
-                bool moreAbove = startIdx > 0;
-                bool moreBelow = startIdx + maxVisibleGround < itemDescriptions.Count;
-                
-                string scrollIndicator = "  ";
-                if (moreAbove && moreBelow) scrollIndicator += "^ (more items above and below) v";
-                else if (moreAbove) scrollIndicator += "^ (more items above)";
-                else if (moreBelow) scrollIndicator += "v (more items below) v";
-
-                Console.WriteLine(scrollIndicator.PadRight(ScreenWidth));
-            }
-            else
-            {
-                Console.WriteLine(new string(' ', ScreenWidth)); 
-            }
+            RenderPopulatedItemList(itemDescriptions, player.SelectedItemIndex, player.IsInventoryActive, maxVisibleGround);
         }
+    }
+
+    private void RenderMessageBar(string? message)
+    {
         if (message != null)
         {
             Console.BackgroundColor = ConsoleColor.Red;
             Console.ForegroundColor = ConsoleColor.White;
             Console.Write(message);
             Console.ResetColor();
-            int padding = ScreenWidth - message.Length;
+            
+            int padding = ScreenWidth - 40 + SidePanelWidth - message.Length;
             if (padding > 0) Console.WriteLine(new string('-', padding));
         }
         else
         {
-            Console.WriteLine(new string('-', 82));
+            Console.WriteLine(new string('-', ScreenWidth - 40 + SidePanelWidth));
         }
+    }
 
+    private void RenderInstructions(List<string> instructions)
+    {
         foreach (var inst in instructions)
         {
-            Console.WriteLine(inst.PadRight(80));
+            Console.WriteLine(inst.PadRight(ScreenWidth));
         }
-        
-        for (int i = 0; i < 3; i++) Console.WriteLine(new string(' ', 80));
     }
 
-    public string Separator()
+    // Helper methods (row rendering)
+
+    private void RenderMapRow(int y, int mapDisplayHeight, int mapDisplayWidth, Room room, Player player)
     {
-        return new string('-', 40);
+        if (y == 0 || y == mapDisplayHeight - 1)
+        {
+            Console.Write("+" + new string('-', Room.Width) + "+");
+        }
+        else if (y > 0 && y < mapDisplayHeight - 1)
+        {
+            int mapY = y - 1;
+            Console.Write("|");
+
+            for (int x = 0; x < Room.Width; x++)
+            {
+                if (x == player.X && mapY == player.Y)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write(player.Symbol);
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.Write(room.GetCell(x, mapY).GetSymbol());
+                }
+            }
+            Console.Write("|");
+        }
+        else
+        {
+            Console.Write(new string(' ', mapDisplayWidth));
+        }
     }
+
+    private void RenderSidePanelRow(int y, List<(string Text, bool Highlight)> sidePanel)
+    {
+        if (y < sidePanel.Count)
+        {
+            var line = sidePanel[y];
+            if (line.Highlight)
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+
+            Console.Write(line.Text.PadRight(40)); 
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.Write(new string(' ', 40));
+        }
+    }
+
+    // Helper methods (item lists) 
+
+    private void RenderEmptyItemList(bool isInventoryActive, int maxVisibleGround)
+    {
+        bool isSelected = !isInventoryActive;
+        if (isSelected)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+        
+        string prefix = isSelected ? "> " : "  ";
+        string textToPrint = $"{prefix}~empty ";
+        Console.Write(textToPrint);
+        Console.ResetColor();
+        
+        int padding = ScreenWidth - " ~empty".Length;
+        if (padding > 0) Console.WriteLine(new string(' ', padding));
+        else Console.WriteLine();
+        
+        for (int i = 1; i < maxVisibleGround; i++) 
+        {
+            Console.WriteLine(new string(' ', ScreenWidth));
+        }
+        Console.WriteLine(new string(' ', ScreenWidth));
+    }
+
+    private void RenderPopulatedItemList(List<string> itemDescriptions, int selectedItemIndex, bool isInventoryActive, int maxVisibleGround)
+    {
+        int startIdx = Math.Max(0, selectedItemIndex - (maxVisibleGround / 2));
+        if (startIdx + maxVisibleGround > itemDescriptions.Count)
+        {
+            startIdx = Math.Max(0, itemDescriptions.Count - maxVisibleGround);
+        }
+
+        for (int i = 0; i < maxVisibleGround; i++)
+        {
+            int itemIdx = startIdx + i;
+            if (itemIdx < itemDescriptions.Count)
+            {
+                bool isSelected = !isInventoryActive && (itemIdx == selectedItemIndex);
+                
+                if (isSelected)
+                {
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+
+                string prefix = isSelected ? "> " : "  ";
+                string textToPrint = $"{prefix}{itemDescriptions[itemIdx]} ";
+                
+                Console.Write(textToPrint);
+                Console.ResetColor();
+
+                int padding = ScreenWidth - textToPrint.Length;
+                if (padding > 0) Console.WriteLine(new string(' ', padding));
+                else Console.WriteLine();
+            }
+            else
+            {
+                Console.WriteLine(new string(' ', ScreenWidth)); 
+            }
+        }
+
+        if (itemDescriptions.Count > maxVisibleGround)
+        {
+            bool moreAbove = startIdx > 0;
+            bool moreBelow = startIdx + maxVisibleGround < itemDescriptions.Count;
+            
+            string scrollIndicator = "  ";
+            if (moreAbove && moreBelow) scrollIndicator += "^ (more items above and below) v";
+            else if (moreAbove) scrollIndicator += "^ (more items above)";
+            else if (moreBelow) scrollIndicator += "v (more items below) v";
+
+            Console.WriteLine(scrollIndicator.PadRight(ScreenWidth));
+        }
+        else
+        {
+            Console.WriteLine(new string(' ', ScreenWidth)); 
+        }
+    }
+
+    // Side panel methods
+
+    public string Separator() => new string('-', SidePanelWidth);
 
     private List<(string Text, bool Highlight)> GenerateSidePanel(Room room, Player player)
     {
         var panel = new List<(string, bool)>();
 
         void Add(string text, bool highlight = false) => panel.Add((text, highlight));
-        
-        string FormatRow(string left, string right) 
-        {
-            return left.PadRight(20) + right;
-        }
+        string FormatRow(string left, string right) => left.PadRight(20) + right;
 
         Add(Separator());
         
         var stats = player.stats.GetAttributes();
-
         Add(FormatRow($"{player.Name}, Stats:", player.money.GetMoney()));
-        Add(stats[0]);
-        Add(stats[1]);
-        Add(stats[2]);
-        Add(stats[3]);
-        Add(stats[4]);
-        Add(stats[5]);
+        
+        foreach (var stat in stats)
+        {
+            Add(stat);
+        }
         
         Add(Separator());
         Add($"Left hand: {player.equipment.GetLeftHandName()}");
@@ -228,14 +258,12 @@ public class Display
         {
             bool isSelected = player.IsInventoryActive;
             string prefix = isSelected ? "> " : "  ";
-            Add(prefix+ "~empty", isSelected);
-            for (int i = 1; i < maxVisible; i++) Add(""); 
+            Add(prefix + "~empty", isSelected);
+            for (int i = 1; i < maxVisible; i++) Add(new string(' ', ScreenWidth)); 
         }
         else
         {
-            int selectedIdx = player.SelectedInventoryIndex;
-            
-            int startIdx = Math.Max(0, selectedIdx - (maxVisible / 2));
+            int startIdx = Math.Max(0, player.SelectedInventoryIndex - (maxVisible / 2));
             if (startIdx + maxVisible > items.Count)
             {
                 startIdx = Math.Max(0, items.Count - maxVisible);
@@ -246,13 +274,13 @@ public class Display
                 int itemIdx = startIdx + i;
                 if (itemIdx < items.Count)
                 {
-                    bool isSelected = player.IsInventoryActive && (itemIdx == selectedIdx);
+                    bool isSelected = player.IsInventoryActive && (itemIdx == player.SelectedInventoryIndex);
                     string prefix = isSelected ? "> " : "  "; 
                     Add(prefix + items[itemIdx], isSelected);
                 }
                 else
                 {
-                    Add("");
+                    Add(new string(' ', ScreenWidth)); 
                 }
             }
         }
